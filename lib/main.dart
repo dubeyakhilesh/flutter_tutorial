a94a01166ee50160a198c76229acf7900334346b
Akhilesh <akhilesh2011.here@gmail.com>
2019-09-25 13:24:37 +0530

    It contains basic widget example.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    Center(
-      child: Text(
-        "Hello flutter developer!!!",
-        textDirection: TextDirection.ltr,
+void main() {
+  runApp(MaterialApp(
+    title: "First App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home",
+          style: TextStyle(),
+        ),
       ),
-    )
-  );
+      body: Material(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Text(
+            "Hello flutter developer!!!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+        ),
+      ),
+    ),
+  ));
 }
